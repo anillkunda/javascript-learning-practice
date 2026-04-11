3aa7bc0d97bbf029bc1ad7ca36918d6c30a406ca
Anil Kunda <anilkunda701@gmail.com>
2026-04-11 16:07:41 +0530

    handle clicks, game logic, and style updates

--- 04-DOM-and-Events/01-Guessing-Numbar/script.js
+++ 04-DOM-and-Events/01-Guessing-Numbar/script.js
@@ -1,1 +1,49 @@
 'use strict';
+
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // TODO: ----- When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+
+    // TODO: ----- When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+
+    // Manipulating CSS Style
+    document.querySelector('body').style.background = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //  TODO: ----- When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // TODO: ----- When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
